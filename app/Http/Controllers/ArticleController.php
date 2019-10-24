4644b15a7a9632f763d76777df718ad14466c079
reeshkeed <girishdaloso@hotmail.com>
2019-10-25 06:53:55 +0800

    Created article api

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Articles;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        return Articles::all();
+    }
+
+    public function show(Articles $article)
+    {
+        return $article;
+    }
+
+    public function store(Request $request)
+    {
+        return Articles::create($request->all());
+
+        return response()->json($article, 201);
+    }
+
+    public function update(Request $request, Articles $article)
+    {
+        $article->update($request->all());
+
+        return response()->json($article, 200);
+    }
+
+    public function delete(Articles $article)
+    {
+        $article->delete();
+
+        return response()->json(null, 204);
+    }
+}
