9cec6c43b5ac8ebc702eef4d8a9b6b0ed8256c39
reeshkeed <girishdaloso@hotmail.com>
2019-10-27 21:32:53 +0800

    Fixed clear cache on testing

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Artisan;
 
 trait CreatesApplication
 {
@@ -17,6 +18,19 @@ trait CreatesApplication
 
         $app->make(Kernel::class)->bootstrap();
 
+        $this->clearCache();
+
         return $app;
+    }
+
+    /**
+     * Clears Laravel Cache.
+     */
+    protected function clearCache()
+    {
+        $commands = ['clear-compiled', 'cache:clear', 'view:clear', 'config:clear', 'route:clear'];
+        foreach ($commands as $command) {
+            \Illuminate\Support\Facades\Artisan::call($command);
+        }
     }
 }
